572adaf18b35fe5fe6da84035f544c8a81430fba
Cassandre Froelicher <cassandre.froelicher@heig-vd.ch>
2024-02-21 15:51:26 +0100

    ocean et pave

---
+++ src/aframe/duplicate-me.js
@@ -0,0 +1,51 @@
+AFRAME.registerComponent('duplicate-me', {
+    schema: {
+      tileSize: {type: 'int', default: 1},
+  
+      cols: {type: 'int', default: 10},
+      rows: {type: 'int', default: 10},  
+      offset: {type: 'int', default: 0},
+    },
+  /**
+   * tileSize : La taille de base d'une tuile (nous garderons la même pour la width et height,
+   * la depth sera attribuée au choix, en dur dans le composant)  
+   * cols : Le nombre de tuiles disposées sur l'aye Z rows : Le nombre de tuiles disposées sur l'aye X
+   * offset : Un paramètre que vous pouvez rajouter en option pour permettre d'ajouter
+   * un écart entre les tuiles sur les axes X et Z (utile pour voir que nos tuiles sont "bien là" et qu'il ne
+   * s'agit pas d'une seule grosse boîte ou d'un simple plan)
+   */
+    init: function () {
+      var tileSize = this.data.tileSize;
+      var cols = this.data.cols;
+      var rows = this.data.rows;
+      var offset = this.data.offset;
+      console.log(this.el.object3D.position);
+      //var sceneEl = this.el.sceneEl;
+      var tileEl;
+      var x, z;
+      for (x = 0; x < rows; x++) {
+        for (z = 0; z < cols; z++) {
+            tileEl = document.createElement('a-box');
+            tileEl.setAttribute('width', tileSize);
+            tileEl.setAttribute('height', tileSize);
+            tileEl.setAttribute('depth', tileSize);
+            tileEl.setAttribute('material', 'color', 'pink');
+            tileEl.setAttribute('position', {
+                x: this.el.object3D.position.x + x * tileSize + offset * x,
+                y: this.el.object3D.position.y + Math.random()*0.1,
+                z: this.el.object3D.position.z + z * tileSize + offset * z
+            });
+            // alternate colors like a chessboard
+            if ((x + z) % 2 === 0) {
+                tileEl.setAttribute('material', 'color', 'black');
+            }
+            // change luminosity randomly for each tile using hsl and changing only the luminosity whil keeping wooden beige as a base color
+            tileEl.setAttribute('material', 'color', 'hsl(25, 100%, ' + (Math.random() * 5 + 70) + '%)');
+            document.querySelector('a-scene').appendChild(tileEl); 
+        }
+      }
+    },
+  
+    
+  
+  });
--- src/components/TheScene.vue
+++ src/components/TheScene.vue
@@ -2,9 +2,8 @@
   import { ref } from 'vue';
 
   import TheCameraRig from './TheCameraRig.vue';
-  import TheMainRoom from './TheMainRoom.vue';
-  import TheLifeCubeRoom from './TheLifeCubeRoom.vue';
-  import ThePhysicRoom from './ThePhysicRoom.vue';
+  import TheOcean from './TheOcean.vue';
+  import '../aframe/duplicate-me.js';
 
   defineProps({
     scale: Number,
@@ -15,51 +14,9 @@
 </script>
 
 <template>
-  <a-scene
-    background="color: black;"
-    :webxr="`
-      requiredFeatures: local-floor;
-      referenceSpaceType: local-floor;
-      optionalFeatures: dom-overlay;
-      overlayElement: ${overlaySelector};
-    `"
-    xr-mode-ui="XRMode: xr"
-    physx="
-      autoLoad: true;
-      delay: 1000;
-      useDefaultScene: false;
-      wasmUrl: lib/physx.release.wasm;
-    "
-  >
-
-    <a-assets @loaded="allAssetsLoaded = true">
-      <!--
-        Title: VR Gallery
-        Model source: https://sketchfab.com/3d-models/vr-gallery-1ac32ed62fdf424498acc146fad31f7e
-        Model author: https://sketchfab.com/mvrc.art (Maxim Mavrichev)
-        Model license: CC BY 4.0 ( https://creativecommons.org/licenses/by/4.0/ )
-      -->
-      <a-asset-item id="room" src="assets/vr_gallery.glb"></a-asset-item>
-      <!--
-        Title: 3D Gallery for VR projects
-        Model source: https://sketchfab.com/3d-models/3d-gallery-for-vr-projects-68f77ed8558c4bd59e0a13e2cc9d1fd1
-        Model author: https://sketchfab.com/tekuto1s (tekuto1s)
-        Model license: CC BY 4.0 ( https://creativecommons.org/licenses/by/4.0/ )
-      -->
-      <a-asset-item id="physic-room" src="assets/3d_gallery_for_vr_projects.glb"></a-asset-item>
-      <a-asset-item id="sound-1" response-type="arraybuffer" src="assets/sound1.mp3" preload="auto"></a-asset-item>
-      <img id="room-physic-out-texture" :src="`assets/main-room-from-physic-room.png`">
-      <img id="room-gol-out-texture" :src="`assets/main-room-from-gol-room.png`">
-      <img id="room-physic-texture" :src="`assets/physicRoom.png`">
-    </a-assets>
-
-    <template v-if="allAssetsLoaded">
-      <TheMainRoom :scale="scale" />
-      <TheLifeCubeRoom />
-      <ThePhysicRoom />
-    </template>
-
-    <TheCameraRig />
-
+  <a-scene stats background="color: #bdd8e5" fog="type: linear; color: #bdd8e5; near: 30; far: 60">   
+      <a-box position="0 0 -10" duplicate-me></a-box>
+      <TheOcean />
+      <TheCameraRig />
   </a-scene>
 </template>
