494c0653f609c646e728da50d0b3ae4a527e9f68
Cassandre Froelicher <cassandre.froelicher@heig-vd.ch>
2024-02-25 00:27:21 +0100

    update colors

--- src/components/TheOcean.vue
+++ src/components/TheOcean.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template>
-    <a-ocean color="#92cee2" width="100" depth="100" amplitude="0" density-amplitude="0.1" opacity="1" density="50" speed="2"></a-ocean>
-    <a-ocean color="#92cee2" width="100" depth="100" amplitude="0" density-amplitude="0.15" opacity="0.5" density="50" speed="2"></a-ocean>
-    <a-entity light="type: ambient; color: #6895a1"></a-entity>
+    <a-ocean color="#92cee2" width="100" depth="100" amplitude="0" density-amplitude="0.1" opacity="1" density="50" speed="1"></a-ocean>
+    <a-ocean color="#92cee2" width="100" depth="100" amplitude="0" density-amplitude="0.15" opacity="1" density="50" speed="1"></a-ocean>
+    <a-entity light="type: ambient; color: #0b3651"></a-entity>
     <a-entity light="type: point; intensity: 2" position="0 2 -10"></a-entity>
 </template>
