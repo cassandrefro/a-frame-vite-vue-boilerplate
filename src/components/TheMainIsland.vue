a24b220c97f6c640d81e9d5051aba38a91f61f77
Cassandre Froelicher <cassandre.froelicher@heig-vd.ch>
2024-02-26 15:42:37 +0100

    update map

---
+++ src/components/TheMainIsland.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+  import '../aframe/life-like-automaton.js';
+
+  defineProps({
+    scale: Number,
+  });
+
+</script>
+
+<template>
+  <a-entity
+    gltf-model="#main-island"
+    position="0 0 0"
+    scale="1 1.1 1"
+  />
+  <a-entity
+    gltf-model="#cauldron"
+    position="0 0 0"
+    scale="1 1.1 1"
+    />
+
+
+</template>
--- src/components/TheScene.vue
+++ src/components/TheScene.vue
@@ -1,9 +1,8 @@
 <script setup>
   import { ref } from 'vue';
-
-  import TheCameraRig from './TheCameraRig.vue';
-  import TheOcean from './TheOcean.vue';
+  import TheCameraRig from './TheCameraRig.vue';  
   import '../aframe/duplicate-me.js';
+import TheMainIsland from './TheMainIsland.vue';
 
   defineProps({
     scale: Number,
@@ -14,9 +13,24 @@
 </script>
 
 <template>
-  <a-scene stats background="color: #bdd8e5" fog="type: linear; color: #bdd8e5; near: 30; far: 60">   
-      <a-box position="-10 0 -10" duplicate-me="rows: 20; cols: 20"></a-box>
-      <TheOcean />
+  <a-scene stats background="color: #bdd8e5" fog="type: linear; color: #bdd8e5; near: 30; far: 90">   
+      <a-box position="-10 -50 -10" duplicate-me="rows: 20; cols: 20"></a-box>
+      <a-entity light="type: point; intensity: 2" position="200 10 0"></a-entity>
+
+      <a-assets @loaded="allAssetsLoaded = true">
+      <!--
+        Title: 
+        Model source: 
+        Model author: (name)
+        Model license: CC BY 4.0 ( https://creativecommons.org/licenses/by/4.0/ )
+      -->
+      <a-asset-item id="main-island" src="assets/main_island_v1.glb"></a-asset-item>  
+      <a-asset-item id="cauldron" src="assets/cauldron_green.glb"></a-asset-item>      
+    </a-assets>
+
+    <template v-if="allAssetsLoaded">
+      <TheMainIsland :scale="scale" />
+    </template>
       <TheCameraRig />
   </a-scene>
 </template>
