8e0a20e8f7fc5695fcde75a0d875de5364076b10
Cassandre Froelicher <cassandre.froelicher@heig-vd.ch>
2024-03-02 17:55:15 +0100

    navigation mesh

--- src/components/TheMainIsland.vue
+++ src/components/TheMainIsland.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-  import '../aframe/life-like-automaton.js';
+import PortalTeleporter from './PortalTeleporter.vue';
 
   defineProps({
     scale: Number,
@@ -14,12 +14,12 @@
     position="0 0 0"
     scale="1 1 1"
   >
-    <a-entity
+   <!--  <a-entity
         geometry="primitive: plane; height: 3; width: 1.5;"
         rotation="0 -70 0"
         position="23.483 0.984 0.876"
         color="#FFF"
-    ></a-entity>
+    ></a-entity> -->
 
     <a-entity
         geometry="primitive: sphere; radius: 400"
@@ -34,7 +34,48 @@
         scale="1 1 1"
     />
 
-  </a-entity>
-  <a-sky color="#FFF" src="assets/sky.png" position="0 -300 0" scale="1.5 1.5 1.5" material="fog: false"></a-sky>  
+    <PortalTeleporter
+      label="Go to the fairy island"
+      life-like-automaton="resolution: 256;"
+      position="23.483 0.984 0.876"
+      rotation="0 -90 0"
+      :rot="false"
+      :x="93.181"
+      :y="-2.994"
+      :z="-7.769"
 
+    />
+
+    <a-sky color="#FFF" src="assets/sky.png" position="0 -300 0" scale="1.5 1.5 1.5" material="fog: false"></a-sky>  
+  </a-entity>
+ 
+ <!-- Main room navigation mesh  -->
+ <!-- <a-entity
+ id="second-room-nav-mesh" 
+    geometry="primitive: circle; radius: 35.610"
+    position="93.181 -2.994 -9.769"
+    rotation="-90 20 0"
+    scale="1.15 1 1"
+    data-role="nav-mesh"
+    material="color: blue"
+    visible="false"
+  ></a-entity> -->
+
+  <a-entity
+ id="second-room-nav-mesh" 
+    gltf-model="#second-mesh"
+    position="94.300 -2.994 -7.860"
+    rotation="0 0 0"
+    scale="1 1 1"
+    data-role="nav-mesh"
+    material="color: blue"
+  ></a-entity>
+
+<!--   <a-entity
+    geometry="primitive: plane; height: 13.5; width: 6"
+    position="0 0 0"
+    rotation="-90 0 0"
+    data-role="nav-mesh"
+    material="color: blue"
+  ></a-entity> -->
 </template>
--- src/components/TheScene.vue
+++ src/components/TheScene.vue
@@ -12,7 +12,7 @@ import TheMainIsland from './TheMainIsland.vue';
 </script>
 
 <template>
-  <a-scene stats fog="type: linear; color: #8c73ab; near: 1; far: 80">  
+  <a-scene stats fog="type: linear; color: #8c73ab; near: 1; far: 200">  
     <!-- <a-scene stats background="color: #232027 #8c73ab #574370" fog="type: linear; color: #232027; near: 50; far: 120">    -->
       <!-- <a-entity light="type: ambient;intensity: 5; color: #4c3c2f" position="1.608 9.227 -25.154"></a-entity> -->
       <a-entity light="type: point; intensity: 0.3; color: #31abb4" position="4.142 5.163 -16.797" rotation="0 -180 0"></a-entity>
@@ -26,6 +26,7 @@ import TheMainIsland from './TheMainIsland.vue';
         Model license: CC BY 4.0 ( https://creativecommons.org/licenses/by/4.0/ )
       -->
       <a-asset-item id="main-island" src="assets/main_island_v1.glb"></a-asset-item>  
+      <a-asset-item id="second-mesh" src="assets/second_mesh.glb"></a-asset-item>
       <a-asset-item id="cauldron" src="assets/cauldron_green.glb"></a-asset-item> 
     </a-assets>
         
